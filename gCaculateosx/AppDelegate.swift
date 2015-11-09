9d33cb565f1afa809caafbea17a762b93d1a7dcf
高小强 <xqgao23@gmail.com>
2015-11-09 15:20:12 +0800

    Initial Commit

---
+++ gCaculateosx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gCaculateosx
+//
+//  Created by 高小强 on 15/11/9.
+//  Copyright © 2015年 高小强. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gCaculateosx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gCaculateosx
+//
+//  Created by 高小强 on 15/11/9.
+//  Copyright © 2015年 高小强. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
